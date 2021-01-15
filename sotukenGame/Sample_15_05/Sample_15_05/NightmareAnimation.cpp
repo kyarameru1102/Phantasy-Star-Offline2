55b69bb63e4e92be9435011d15d795f03f37fcfc
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2021-01-15 12:14:46 +0900

    NightmareAnimationを追加

--- sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.h"
 #include "NightmareAnimation.h"
+
+NightmareAnimation::NightmareAnimation()
+{
+
+}
+
+NightmareAnimation::~NightmareAnimation()
+{
+
+}
--- sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.h
+++ sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.h
@@ -1,5 +1,46 @@
 #pragma once
-class NightmareAnimation
-{
+const enum NightmAnim {
+	enIdle,                    //待機
+	enWalk,                    //歩き
+	enRun,                     //走り
+	enScream,                  //咆哮
+	enAttack,                  //攻撃
+	enHornattack,              //角攻撃
+	enGethit,                  //被ダメ
+	enDie,                     //死亡
+	enNightmAnimClip_num
 };
+class NightmareAnimation : public IGameObject
+{
+public:
+	/// <summary>
+	/// コンストラクタ。
+	/// </summary>
+	NightmareAnimation();
+	/// <summary>
+	/// デストラクタ。
+	/// </summary>
+	~NightmareAnimation();
+	/// <summary>
+	/// スタート関数。
+	/// </summary>
+	/// <returns></returns>
+	bool Start() override;
+	/// <summary>
+	/// 更新関数。
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// アニメーションクリップを返す。
+	/// </summary>
+	/// <returns></returns>
+	AnimationClip* GetAnimationClip()
+	{
+		return animClip;
+	}
+private:
+	AnimationClip animClip[enNightmAnimClip_num];//アニメーションクリップ。
+};
+
+
 
